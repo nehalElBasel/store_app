56d5772e6c3debe871ea8ac3170252102e265b06
nehalElBasel <nehalkhaledbusiness@gmail.com>
2025-08-24 08:46:16 +0300

    fix get all products service

--- lib/services/get_products_service.dart
+++ lib/services/get_products_service.dart
@@ -1,8 +1,18 @@
 import 'package:simple_store_app/http_api.dart';
+import 'package:simple_store_app/models/product_model.dart';
 
 class GetProductService {
   final url = "/products";
-  getProducts() async {
-    List<dynamic> products = await HttpApi().get(url);
+  Future<List<ProductModel>> getProducts() async {
+    try {
+      List<ProductModel> products = [];
+      List<dynamic> dynamicProducts = await HttpApi().get(url);
+      dynamicProducts.forEach((item) {
+        products.add(ProductModel.fromJson(item));
+      });
+      return products;
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 }
