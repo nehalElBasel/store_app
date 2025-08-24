37447a667ca7b67642a9e85eba3c7508b26aa9ec
nehalElBasel <nehalkhaledbusiness@gmail.com>
2025-08-24 08:26:45 +0300

    upload app files

---
+++ lib/http_api.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class HttpApi {
+  final String baseUrl = "https://fakestoreapi.com";
+
+  Future<dynamic> get(String url) async {
+    http.Response response = await http.get(Uri.parse("$baseUrl$url"));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception(
+        "there is an error with status ${response.statusCode} and body ${response.body}",
+      );
+    }
+  }
+}
